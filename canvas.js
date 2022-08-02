553d3e32161437f10fb26a3706fbb1dfdaba90aa
nj260701 <neeraj.nj.260701@gmail.com>
2022-08-02 15:46:21 +0530

    Download ,Eraser, pencils functionality finished

--- canvas.js
+++ canvas.js
@@ -1,11 +1,23 @@
 let canvas = document.querySelector("canvas");
-
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+let pencilColorEle = document.querySelectorAll(".pencil-color");
+let pencilWidthEle = document.querySelector(".pencil-width");
+let eraserWidthEle = document.querySelector(".eraser-width");
+
+let download = document.querySelector(".download");
+
+
+let pencilColor = "black";
+let eraserColor = "white";
+let pencilWidth = pencilWidthEle.value;
+let eraserWidth = eraserWidthEle.value;
+
+let undoRedoTracker = [];
+let track = 0;
+
 mouseDown = false
-
-
 
 //API
 let tool = canvas.getContext("2d");
@@ -20,45 +32,83 @@ let tool = canvas.getContext("2d");
 // tool.lineTo(1000,500); //ending point
 // tool.stroke(); //fill color
 
-tool.strokeStyle = "red";
-tool.lineWidth = "3";
+tool.strokeStyle = pencilColor;
+tool.lineWidth = pencilWidth;
 
 //mouseDown ---> Start a new path/graphics (beginPath() , moveTo())
 //mouseMove ---> path fill (lineTo(), stroke())
 //mouseUp ---> end the began path (lineTo());
 
 
-canvas.addEventListener("mousedown",(e)=>{
+canvas.addEventListener("mousedown", (e) => {
     mouseDown = true;
     beginPath({
-        x:e.clientX ,
-        y:e.clientY 
+        x: e.clientX,
+        y: e.clientY
     });
 })
 
-canvas.addEventListener("mousemove",(e)=>{
-    if(mouseDown){
+canvas.addEventListener("mousemove", (e) => {
+    if (mouseDown) {
         drawStroke({
-            x:e.clientX ,
-            y:e.clientY
+            x: e.clientX,
+            y: e.clientY
         })
     }
-    
+
 })
 
-canvas.addEventListener("mouseup" ,(e)=>{
+canvas.addEventListener("mouseup", (e) => {
     mouseDown = false;
 })
 
-function beginPath(strokeObj){
+function beginPath(strokeObj) {
     tool.beginPath();
-    tool.moveTo(strokeObj.x ,strokeObj.y);
+    tool.moveTo(strokeObj.x, strokeObj.y);
 }
 
-function drawStroke(strokeObj){
-    tool.lineTo(strokeObj.x ,strokeObj.y);
+function drawStroke(strokeObj) {
+    tool.lineTo(strokeObj.x, strokeObj.y);
     tool.stroke();
 }
 
+pencilColorEle.forEach((colorElem) => {
+    colorElem.addEventListener("click", (e) => {
+        let color = colorElem.classList[0];
+        pencilColor = color;
+        tool.strokeStyle = pencilColor;
+        tool.lineWidth = pencilWidth
+    })
 
+})
+
+pencilWidthEle.addEventListener("change", (e) => {
+    pencilWidth = pencilWidthEle.value;
+    tool.lineWidth = pencilWidth
+    tool.strokeStyle = pencilColor
+})
+
+eraserWidthEle.addEventListener("change", (e) => {
+    eraserWidth = eraserWidthEle.value;
+    tool.lineWidth = eraserWidth;
+    tool.strokeStyle = eraserColor;
+})
+
+eraser.addEventListener("click", (e) => {
+    if (eraserFlag) {
+        tool.strokeStyle = eraserColor;
+        tool.lineWidth = eraserWidth;
+    } else {
+        tool.strokeStyle = pencilColor;
+        tool.lineWidth = pencilWidth;
+    }
+})
+
+download.addEventListener("click", (e) => {
+    let url = canvas.toDataURL();
+    let a = document.createElement("a");
+    a.href = url;
+    a.download = "openBoard.jpg";
+    a.click();
+})
 
